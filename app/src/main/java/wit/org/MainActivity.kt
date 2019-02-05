bf553230949a9ffbcf7dc4d83523cd035fc276a4
20002355 <seanstokestemp1@gmail.com>
2019-02-05 17:06:11 +0000

    updated design

--- app/src/main/java/wit/org/MainActivity.kt
+++ app/src/main/java/wit/org/MainActivity.kt
@@ -2,11 +2,27 @@ package wit.org
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        info("placemark Activity started..")
         setContentView(R.layout.activity_main)
+        btnAdd.setOnClickListener() {
+            val placemarkTitle = placemarkTitle.text.toString()
+
+            if (placemarkTitle.isNotEmpty()) {
+
+                info("add Button Pressed. $placemarkTitle")
+            } else {
+                toast("please Enter a title")
+            }
+
+        }
     }
 }
